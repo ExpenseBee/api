b6a9f89d72e9bc94887c6bb4930a6a0f7999c3ed
Wagner Guilherme <guilhermeramos31.gr@gmail.com>
2024-07-27 17:29:02 -0300

    feat: add policy stateless

--- src/main/java/com/expensebee/api/configuration/WebSecurityConfig.java
+++ src/main/java/com/expensebee/api/configuration/WebSecurityConfig.java
@@ -4,17 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    return http
-      .authorizeHttpRequests(authorize -> authorize
-        .requestMatchers(HttpMethod.GET,"/user").permitAll()
-        .anyRequest().authenticated())
-      .build();
+    http.csrf(AbstractHttpConfigurer::disable);
+    http.authorizeHttpRequests(authorize -> authorize
+        .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
+        .anyRequest().authenticated()
+      );
+    http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    return http.build();
   }
 }
