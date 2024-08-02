f484f7045b74e9d42f34c3ac4f8170b586f0bf50
Wagner Guilherme <guilhermeramos31.gr@gmail.com>
2024-08-02 01:47:11 -0300

    feat: added swagger configuration and exemples

--- src/main/java/com/expensebee/api/auth/AuthController.java
+++ src/main/java/com/expensebee/api/auth/AuthController.java
@@ -4,6 +4,11 @@ import com.expensebee.api.auth.dto.LoginDTO;
 import com.expensebee.api.auth.dto.LoginResponseDTO;
 import com.expensebee.api.user.dto.CreateUserRequestDTO;
 import com.expensebee.api.user.dto.UserResponseDTO;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,14 +20,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("auth")
+@Tag(name = "Auth", description = "Where user creation and authentication will occur!")
 public class AuthController {
   private final AuthServiceImpl authService;
 
+  @Operation(summary = "Create new user!")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "201", description = "User created successfully!"),
+    @ApiResponse(responseCode = "409", description = "User already exists!"),
+  })
   @PostMapping("create")
   public ResponseEntity<UserResponseDTO> save(@RequestBody CreateUserRequestDTO userRequest){
     return ResponseEntity.status(HttpStatus.CREATED).body(authService.register(userRequest));
   }
 
+  @Operation(summary = "Log in the user!")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Login successful!"),
+    @ApiResponse(responseCode = "404", description = "User not exist!")
+  })
   @PostMapping("login")
   public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginDTO loginRequest){
     return ResponseEntity.status(HttpStatus.OK).body(authService.login(loginRequest));
