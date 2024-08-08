6efd883dda95c014496fd6c45a6aae4065d53612
Wagner Guilherme <guilhermeramos31.gr@gmail.com>
2024-08-07 22:10:24 -0300

    feat: delete import non used

--- src/main/java/com/expensebee/api/user/UserController.java
+++ src/main/java/com/expensebee/api/user/UserController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
