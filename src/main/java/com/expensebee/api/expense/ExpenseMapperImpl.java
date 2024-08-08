fc9a5b24bc456bc0d81d79b475cd6d6d8f912f63
Wagner Guilherme <guilhermeramos31.gr@gmail.com>
2024-08-08 08:55:55 -0300

    feat: added methods in expense mapper

--- src/main/java/com/expensebee/api/expense/ExpenseMapperImpl.java
+++ src/main/java/com/expensebee/api/expense/ExpenseMapperImpl.java
@@ -1,12 +1,32 @@
 package com.expensebee.api.expense;
 
+import com.expensebee.api.expense.dto.ExpenseReqDTO;
+import com.expensebee.api.expense.dto.ExpenseResDTO;
+import com.expensebee.api.expense.entity.Expense;
 import com.expensebee.api.expense.interfaces.ExpenseMapper;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 public class ExpenseMapperImpl implements ExpenseMapper {
   private final ModelMapper mapper;
+
+  @Override
+  public Expense toModel(ExpenseReqDTO expenseReqDTO) {
+    return mapper.map(expenseReqDTO, Expense.class);
+  }
+
+  @Override
+  public ExpenseResDTO ToDTO(Expense expense) {
+    return mapper.map(expense, ExpenseResDTO.class);
+  }
+
+  @Override
+  public List<ExpenseResDTO> ToDTO(List<Expense> expenseList) {
+    return expenseList.stream().map(expense -> mapper.map(expense, ExpenseResDTO.class)).toList();
+  }
 }
--- src/main/java/com/expensebee/api/expense/interfaces/ExpenseMapper.java
+++ src/main/java/com/expensebee/api/expense/interfaces/ExpenseMapper.java
@@ -1,4 +1,13 @@
 package com.expensebee.api.expense.interfaces;
 
+import com.expensebee.api.expense.dto.ExpenseReqDTO;
+import com.expensebee.api.expense.dto.ExpenseResDTO;
+import com.expensebee.api.expense.entity.Expense;
+
+import java.util.List;
+
 public interface ExpenseMapper {
+  Expense toModel(ExpenseReqDTO expenseReqDTO);
+  ExpenseResDTO ToDTO(Expense expense);
+  List<ExpenseResDTO> ToDTO(List<Expense> expenseList);
 }
