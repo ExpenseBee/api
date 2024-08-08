62f2c1046139c9188fa9e7a1390e21009a022757
Wagner Guilherme <guilhermeramos31.gr@gmail.com>
2024-08-07 23:47:01 -0300

    refactor: replacing uow with user repository

--- src/main/java/com/expensebee/api/unitOfWork/interfaces/UnitUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.expensebee.api.unitOfWork.interfaces;
-
-import com.expensebee.api.unitOfWork.repository.RoleRepository;
-import com.expensebee.api.unitOfWork.repository.UserRepository;
-
-public interface UnitUserRepository {
-  UserRepository getUserRepository();
-}
