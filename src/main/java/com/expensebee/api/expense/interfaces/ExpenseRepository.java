f9ff6f53fe387a9486a9c9a71511f1eb7a15862e
Wagner Guilherme <guilhermeramos31.gr@gmail.com>
2024-08-08 09:15:46 -0300

    feat: added methods in expense repository

--- src/main/java/com/expensebee/api/expense/interfaces/ExpenseRepository.java
+++ src/main/java/com/expensebee/api/expense/interfaces/ExpenseRepository.java
@@ -1,4 +1,16 @@
 package com.expensebee.api.expense.interfaces;
 
+import com.expensebee.api.expense.dto.ExpenseResDTO;
+import com.expensebee.api.expense.entity.Expense;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ExpenseRepository {
+  Optional<Expense> findById(UUID id);
+  List<Expense> findAll();
+  Expense save(Expense expense);
+  Expense update(Expense expense);
+  void deleteById(UUID id);
 }
