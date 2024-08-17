ca9303da46e5bf3b29a0282396573f17c87b3cc2
Wagner Ramos <guilhermeramos31.gr@gmail.com>
2024-08-16 22:25:20 -0300

    remove: unused dependencies

--- src/main/java/com/expensebee/api/auth/AuthServiceImpl.java
+++ src/main/java/com/expensebee/api/auth/AuthServiceImpl.java
@@ -21,8 +21,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
